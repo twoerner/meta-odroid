a0eaf01a1ada2e3d2c09dad7ac60280bdf8ac14e
Armin Kuster <akuster808@gmail.com>
2019-04-28 16:52:46 -0700

    testing travis builds:
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Bootstrapper for buildbot slave
+
+DIR="build"
+MACHINE="odroid-c2"
+CONFFILE="conf/auto.conf"
+BITBAKEIMAGE="core-image-full-cmdline"
+
+# clean up the output dir
+echo "Cleaning build dir"
+rm -rf $DIR
+
+# Reconfigure dash on debian-like systems
+which aptitude > /dev/null 2>&1
+ret=$?
+if [ "$(readlink /bin/sh)" = "dash" -a "$ret" = "0" ]; then
+  sudo aptitude install expect -y
+  expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
+elif [ "${0##*/}" = "dash" ]; then
+  echo "dash as default shell is not supported"
+  return
+fi
+# bootstrap OE
+echo "Init OE"
+export BASH_SOURCE="openembedded-core/oe-init-build-env"
+. ./openembedded-core/oe-init-build-env $DIR
+
+# Symlink the cache
+#echo "Setup symlink for sstate"
+#ln -s ~/sstate/${MACHINE} sstate-cache
+
+# add the missing layers
+echo "Adding layers"
+bitbake-layers add-layer ../meta-odroid
+
+# fix the configuration
+echo "Creating auto.conf"
+
+if [ -e $CONFFILE ]; then
+    rm -rf $CONFFILE
+fi
+cat <<EOF > $CONFFILE
+MACHINE = "${MACHINE}"
+#IMAGE_FEATURES += "tools-debug"
+#IMAGE_FEATURES += "tools-tweaks"
+#IMAGE_FEATURES += "dbg-pkgs"
+# rootfs for debugging
+#IMAGE_GEN_DEBUGFS = "1"
+#IMAGE_FSTYPES_DEBUGFS = "tar.gz"
+EXTRA_IMAGE_FEATURES_append = " ssh-server-dropbear"
+EXTRA_IMAGE_FEATURES_append = " package-management"
+PACKAGECONFIG_append_pn-qemu-native = " sdl"
+PACKAGECONFIG_append_pn-nativesdk-qemu = " sdl"
+USER_CLASSES ?= "buildstats buildhistory buildstats-summary image-mklibs image-prelink"
+require conf/distro/include/no-static-libs.inc
+require conf/distro/include/yocto-uninative.inc
+require conf/distro/include/security_flags.inc
+INHERIT += "uninative"
+HOSTTOOLS_NONFATAL_append = " ssh"
+EOF
+
+echo "To build an image run"
+echo "-------------------------------"
+echo "bitbake core-image-full-cmdline"
+echo "-------------------------------"
+
+
